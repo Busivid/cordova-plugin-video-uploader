a3fec73d09bf3f9b47d5083f8987b33a4f416226
Cory Thompson <coryjthompson@gmail.com>
2017-07-27 14:27:44 +1000

    Fix out of order uploading race condition when using two cached transcoding

--- src/ios/VideoUploader.m
+++ src/ios/VideoUploader.m
@@ -17,10 +17,10 @@
  */
 - (void) cleanUp:(CDVInvokedUrlCommand*)cmd {
     NSLog(@"[VideoUploader]: cleanUp called");
-
+    
     [self.commandDelegate runInBackground:^{
         NSFileManager *fileMgr = [NSFileManager defaultManager];
-
+        
         // delete compressed files from cache directory.
         NSString *cacheDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
         NSString *videoDir = [cacheDir stringByAppendingPathComponent:@"mp4"];
@@ -30,7 +30,7 @@
                 [fileMgr removeItemAtPath:[videoDir stringByAppendingPathComponent:filename] error:NULL];
             }
         }
-
+        
         // delete temp files from picker / capture interface.
         NSString *tempDir = [NSTemporaryDirectory()stringByStandardizingPath];
         NSArray *tempFilesArray = [fileMgr contentsOfDirectoryAtPath:tempDir error:nil];
@@ -39,17 +39,17 @@
                 [fileMgr removeItemAtPath:[tempDir stringByAppendingPathComponent:tempFile] error:NULL];
             }
         }
-
+        
         [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:cmd.callbackId];
     }];
 }
 
 - (void)compressAndUpload:(CDVInvokedUrlCommand*)cmd {
     NSLog(@"[VideoUploader]: compressAndUpload called");
-
+    
     completedTransfers = [[NSMutableArray alloc] init];
     latestCallbackId = cmd.callbackId;
-
+    
     [self.commandDelegate runInBackground:^{
         NSArray *fileOptions = [cmd.arguments objectAtIndex:0];
         for(NSDictionary *options in fileOptions) {
@@ -67,7 +67,7 @@
             NSURL *transcodingSrc = [NSURL URLWithString:options[@"filePath"]];
             NSURL *uploadCompleteUrl = [NSURL URLWithString:options[@"callbackUrl"]];
             NSURL *uploadUrl = [NSURL URLWithString:options[@"uploadUrl"]];
-
+            
             // Initialise UploadOperation which is added to UploadQueue on completetionBlock of transcoding operation
             UploadOperation *uploadOperation = [[UploadOperation alloc] initWithOptions:options commandDelegate:self.commandDelegate cordovaCallbackId:latestCallbackId];
             [uploadOperation setUploadCompleteUrl:uploadCompleteUrl];
@@ -100,38 +100,42 @@
             TranscodeOperation *transcodeOperation = [[TranscodeOperation alloc] initWithFilePath:transcodingSrc dst:transcodingDst options:options commandDelegate:self.commandDelegate cordovaCallbackId:latestCallbackId];
             __weak TranscodeOperation* weakTranscodeOperation = transcodeOperation;
             [transcodeOperation setCompletionBlock:^{
-                if (weakTranscodeOperation.isCancelled){
-                    return;
-                }
-                
-                if (weakTranscodeOperation.errorMessage != nil) {
-                    // Notify cordova a single transcode errored.
-                    [self reportProgress:latestCallbackId progress:[NSNumber numberWithInt:100] progressId:progressId type:@"TRANSCODE_ERROR"];
-                
-                    // Transcoded failed, use original file in upload
-                    [uploadOperation setSource:transcodingSrc];
-                } else {
-                	// Notify cordova a single transcode is complete
-                	[self reportProgress:latestCallbackId progress:[NSNumber numberWithInt:100] progressId:progressId type:@"TRANSCODE_COMPLETE"];
-
-                    NSFileManager *fileMgr = [NSFileManager defaultManager];
-                    unsigned long long transcodingDstFileSize = [[fileMgr attributesOfItemAtPath:transcodingDst.path error:nil] fileSize];
-
-                    unsigned long long transcodingSrcFileSize = [[fileMgr attributesOfItemAtPath:transcodingSrc.path error:nil] fileSize];
+                // Mutex lock to fix race conditions.
+                // If two task have already been transcoded, therefore return instantly, the UploadOperations can be added out of order and looks confusing on the UI.
+                @synchronized (uploadQueue) {
+                    if (weakTranscodeOperation.isCancelled){
+                        return;
+                    }
                     
-                    // Upload the smaller file size
-                    NSURL *fileToUpload = transcodingSrcFileSize >= transcodingDstFileSize
-                    	? transcodingDst
-                    	: transcodingSrc;
+                    if (weakTranscodeOperation.errorMessage != nil) {
+                        // Notify cordova a single transcode errored.
+                        [self reportProgress:latestCallbackId progress:[NSNumber numberWithInt:100] progressId:progressId type:@"TRANSCODE_ERROR"];
+                        
+                        // Transcoded failed, use original file in upload
+                        [uploadOperation setSource:transcodingSrc];
+                    } else {
+                        // Notify cordova a single transcode is complete
+                        [self reportProgress:latestCallbackId progress:[NSNumber numberWithInt:100] progressId:progressId type:@"TRANSCODE_COMPLETE"];
+                        
+                        NSFileManager *fileMgr = [NSFileManager defaultManager];
+                        unsigned long long transcodingDstFileSize = [[fileMgr attributesOfItemAtPath:transcodingDst.path error:nil] fileSize];
+                        
+                        unsigned long long transcodingSrcFileSize = [[fileMgr attributesOfItemAtPath:transcodingSrc.path error:nil] fileSize];
+                        
+                        // Upload the smaller file size
+                        NSURL *fileToUpload = transcodingSrcFileSize >= transcodingDstFileSize
+                        ? transcodingDst
+                        : transcodingSrc;
+                        
+                        // Use transcoded file in upload
+                        [uploadOperation setSource:fileToUpload];
+                    }
                     
-                    // Use transcoded file in upload
-                    [uploadOperation setSource:fileToUpload];
+                    // Add uploading to queue
+                    [uploadQueue addOperation:uploadOperation];
                 }
-                
-                // Add uploading to queue
-                [uploadQueue addOperation:uploadOperation];
             }];
-
+            
             [transcodingQueue addOperation:transcodeOperation];
         }
     }];
@@ -148,27 +152,27 @@
  */
 - (void) handleFatalError:(NSString*)message withCallbackId:(NSString*)callbackId {
     NSLog(@"[VideoUploader]: handleFatalError called");
-
+    
     NSMutableDictionary *results = [NSMutableDictionary dictionaryWithCapacity:2];
     [results setObject:message forKey:@"message"];
     [results setObject:completedTransfers forKey:@"completedTransfers"];
-
+    
     [transcodingQueue cancelAllOperations];
     [uploadQueue cancelAllOperations];
     if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateBackground) {
-
-    	// If we are in the background, display the error message as a local push notification
+        
+        // If we are in the background, display the error message as a local push notification
         UILocalNotification *notification = [[UILocalNotification alloc]init];
-    	notification.alertAction = nil;
-    	notification.soundName = UILocalNotificationDefaultSoundName;
-    	notification.alertBody = message;
-    	notification.soundName = UILocalNotificationDefaultSoundName;
-    	notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:5];
-    	notification.repeatInterval = 0;
-
-    	[[UIApplication sharedApplication]scheduleLocalNotification:notification];
+        notification.alertAction = nil;
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        notification.alertBody = message;
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:5];
+        notification.repeatInterval = 0;
+        
+        [[UIApplication sharedApplication]scheduleLocalNotification:notification];
     }
-
+    
     [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:results] callbackId:callbackId];
 }
 
@@ -185,7 +189,7 @@
     return videoOutput;
 }
 
-- (void) reportProgress:(NSString*)callbackId progress:(NSNumber*)progress progressId:(NSString*)progressId type:(NSString*)type {
+- (void)reportProgress:(NSString*)callbackId progress:(NSNumber*)progress progressId:(NSString*)progressId type:(NSString*)type {
     NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] init];
     [dictionary setValue: progress forKey: @"progress"];
     [dictionary setValue: progressId forKey: @"progressId"];
@@ -197,7 +201,7 @@
 
 - (void) removeBackgroundTask {
     NSLog(@"[VideoUploader]: removeBackgroundTask called");
-
+    
     if (backgroundTaskID != UIBackgroundTaskInvalid) {
         [[UIApplication sharedApplication] endBackgroundTask:backgroundTaskID];
         backgroundTaskID = UIBackgroundTaskInvalid;
@@ -210,32 +214,32 @@
 
 - (void) pluginInitialize {
     NSLog(@"[VideoUploader]: pluginInitalize called");
-
+    
     backgroundTaskID = UIBackgroundTaskInvalid;
-
+    
     transcodingQueue = [[NSOperationQueue alloc] init];
     transcodingQueue.maxConcurrentOperationCount = 1;
-
+    
     uploadQueue = [[NSOperationQueue alloc] init];
     uploadQueue.maxConcurrentOperationCount = 1;
 }
 
 - (void)applicationWillEnterForeground:(UIApplication*)application {
     NSLog(@"[VideoUploader]: applicationWillEnterForeground called");
-
+    
     [self removeBackgroundTask];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     NSLog(@"[VideoUploader]: applicationDidEnterBackground called");
-
+    
     // if stuff in queues, request a background task.
     if ([transcodingQueue operationCount] > 0 || [uploadQueue operationCount] > 0) {
-    	backgroundTaskID = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        backgroundTaskID = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
             NSString* errorMessage = @"Application was running too long in the background and iOS cancelled uploading. Please try again.";
             [self handleFatalError:errorMessage withCallbackId:latestCallbackId];
             [self removeBackgroundTask];
-    	}];
+        }];
     }
 }
 @end
