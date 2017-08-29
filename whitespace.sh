6aab63662d1ed0b5cb0da78a1c52bde91985ab05
root <root@ryan.dev.busivid.com>
2017-08-29 12:20:33 +1000

    Whitespace

--- whitespace.sh
+++ whitespace.sh
@@ -13,10 +13,10 @@ if [ "${OS}" = "Darwin" ]; then
 fi
 
 echo "Removing trailing whitespace"
-find . -type f -not -path './.git/*' -exec ${SED} 's/[[:space:]]+$//g' {} \;
+find . -type f -not -path './.git/*' -name *.h -or -name *.java -or -name *.js -or -name *.m -exec ${SED} 's/[[:space:]]+$//g' {} \;
 
 echo "Removing multiple blank lines"
-find . -type f -not -path './.git/*' -exec ${SED} '/./,/^$/!d' {} \;
+find . -type f -not -path './.git/*' -name *.h -or -name *.java -or -name *.js -or -name *.m -exec ${SED} '/./,/^$/!d' {} \;
 
 echo "Converting line endings"
-find . -type f -not -path './.git/*' -exec dos2unix {} \; > /dev/null 2>&1
+find . -type f -not -path './.git/*' -name *.h -or -name *.java -or -name *.js -or -name *.m -exec dos2unix {} \; > /dev/null 2>&1
